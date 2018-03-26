9bfd169dd8409911ffd0b5da667cfe1a1043a08d
icodewp <31495509+icodewp@users.noreply.github.com>
2018-03-25 23:33:32 -0400

    Add files via upload

--- src/main/java/com/google/demo/realtimeingest/CreateTopicAndPublishMessages.java
+++ src/main/java/com/google/demo/realtimeingest/CreateTopicAndPublishMessages.java
@@ -82,8 +82,8 @@ public class CreateTopicAndPublishMessages {
 		// [END pubsub_publish]
 	}
 
-	public static void main(String... args) throws Exception {
+	/*public static void main(String... args) throws Exception {
 		// createTopic();
 		publishMessages();
-	}
+	}*/
 }
